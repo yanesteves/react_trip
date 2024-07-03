7fa7e2a8f7a4795de3d132e0978f46449d306371
Yan Esteves <yan.m.esteves@gmail.com>
2024-07-02 21:33:34 -0300

    Adicionando evento de clique em um botao de comprar

--- src/components/CardBicicleta.jsx
+++ src/components/CardBicicleta.jsx
@@ -1,12 +1,25 @@
 import './CardBicicleta.css'
 
 function CardBicicleta() {
+
+    function comprarBike() {
+        console.log('comprando a bicicleta')
+    }
+
+    // function teclaPressionada(e) {
+
+    // }
+
     return (
         <>
             <div className="card-bicicleta">
                 <img src="https://www.origamid.com/projetos/bikcraft/img/bicicletas/magic-home.jpg" alt="Imagem da bike" />
                 <h4 className='modelo'>Magic Might</h4>
                 <h6 className='preco'>R$ 2499</h6>
+
+                {/* <input type='text' placeholder='Digite aqui' onChange={(e) => teclaPressionada(e)}></input> */}
+
+                <button onClick={() => comprarBike()}>Comprar</button>
             </div>
         </>
     )
